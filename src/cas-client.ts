87b3a7392908977911840f83565d65dfb30ca069
Chengzhong Wu <chengzhong.wu@redhome.cc>
2016-01-28 22:54:11 +0800

    lint code

--- src/cas-client.ts
+++ src/cas-client.ts
@@ -4,7 +4,7 @@
 
 import * as request from 'superagent';
 import _ = require('lodash');
-import {ServerRequest, ServerResponse, IncomingMessage} from 'http';
+import {ServerRequest, ServerResponse} from 'http';
 import {parse, format, Url} from 'url';
 import {parseString} from 'xml2js';
 
@@ -22,21 +22,21 @@ export interface IClientOptions {
 }
 
 export interface IValidationResult {
-    // user_name, e.g. '老王'
+    // `user_name`, e.g. '老王'
     staffName: string;
-    // userName, e.g. '14111111'
+    // `userName`, e.g. '14111111'
     staffId: string;
-    // id_type, e.g. '1'
+    // `id_type`, e.g. '1'
     idType: string;
-    // unit_id, e.g. '05'
+    // `unit_id`, e.g. '05'
     unitId: string;
-    // unit_name, e.g. '计算机学院'
+    // `unit_name`, e.g. '计算机学院'
     unitName: string;
-    // user_id, e.g. '2014111111111'
+    // `user_id`, e.g. '2014111111111'
     userId: string;
-    // user_sex, e.g. '1'
+    // `user_sex`, e.g. '1'
     userSex: string;
-    // classid, e.g. '14222222'
+    // `classid`, e.g. '14222222'
     classId: string;
 }
 
@@ -51,19 +51,26 @@ export class Client {
     validationCallback: (error: any, response: IValidationResult, request: IRequest) => void;
     callbackProtocol: Protocol;
 
-    constructor({casUrl, validationServiceUrl, validationCallback, callbackProtocol}: IClientOptions) {
+    constructor({
+        casUrl,
+        validationServiceUrl,
+        validationCallback,
+        callbackProtocol
+        }: IClientOptions) {
         this.casUrl = casUrl;
         this.validationServiceUrl = parse(validationServiceUrl);
         this.validationCallback = validationCallback;
         this.callbackProtocol = callbackProtocol;
     }
 
-    validate(ticket: string, serviceUrl: string, callback: (err: any, res: request.Response) => void) {
+    validate(ticket: string,
+             serviceUrl: string,
+             callback: (err: any, res: request.Response) => void) {
         let validationUrl = _.cloneDeep(this.validationServiceUrl);
         validationUrl.query = {};
-        validationUrl.query['ticket'] = ticket;
-        validationUrl.query['service'] = serviceUrl;
-        
+        validationUrl.query.ticket = ticket;
+        validationUrl.query.service = serviceUrl;
+
         request.get(format(validationUrl)).end(callback);
     }
 
@@ -71,22 +78,22 @@ export class Client {
         const protocol = Protocol[this.callbackProtocol];
         const path = req.url.split('?', 2)[0];
         const url = format({ protocol, host: req.headers.host, pathname: path });
-        const ticket = req.query['ticket'];
+        const ticket = req.query.ticket;
 
         if ('undefined' === typeof ticket) {
             res.statusCode = 302;
             res.setHeader('Location', this.casUrl + '?service=' + encodeURIComponent(url));
             return res.end();
         }
-        
+
         this.validate(ticket, url, (err, response) => {
             if (err || !response.ok) {
                 return this.validationCallback(err, null, { req, res, next });
             }
 
-            parseString(response.text, (err, xml) => {
-                if (err) {
-                    return this.validationCallback(err, null, { req, res, next });
+            parseString(response.text, (error, xml) => {
+                if (error) {
+                    return this.validationCallback(error, null, { req, res, next });
                 }
                 const attributes = <any>_.get(xml,
                     ['sso:serviceResponse',
@@ -94,24 +101,24 @@ export class Client {
                         'sso:attributes', '0',
                         'sso:attribute']);
                 if (attributes) {
-                    let attr = {};
+                    let attr = <any> {};
                     for (let item of attributes) {
-                        attr[item['$'].name] = item['$'].value;
+                        attr[item.$.name] = item.$.value;
                     }
                     const result = {
-                        staffName: attr['user_name'],
-                        staffId: attr['userName'],
-                        idType: attr['id_type'],
-                        unitId: attr['unit_id'],
-                        unitName: attr['unit_name'],
-                        userId: attr['user_id'],
-                        userSex: attr['user_sex'],
-                        classId: attr['classid'],
+                        classId: attr.classid,
+                        idType: attr.id_type,
+                        staffId: attr.userName,
+                        staffName: attr.user_name,
+                        unitId: attr.unit_id,
+                        unitName: attr.unit_name,
+                        userId: attr.user_id,
+                        userSex: attr.user_sex,
                     };
                     this.validationCallback(null, result, { req, res, next });
                 } else {
-                    const error = new TypeError('CAS validation response parsing error');
-                    this.validationCallback(error, null, { req, res, next})
+                    const e = new TypeError('CAS validation response parsing error');
+                    this.validationCallback(e, null, { req, res, next });
                 }
             });
         });
